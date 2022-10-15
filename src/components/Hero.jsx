560b1ec4d07d55b0860bc5f23343df0a65b32d9c
jmadrigaladan <jorgemadrigaladan@gmail.com>
2022-10-14 23:39:33 -0700

    added a divider section for discover section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Nav from "../components/Nav.jsx";
+import heroImage from "../assets/undraw_imagination_re_i0xi 1.svg";
+import "./Hero.css";
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <Nav />
+      <div className="hero__container">
+        <div className="hero__wrapper">
+          <figure className="img__container">
+            <img src={heroImage} alt="" className="heroImage" />
+          </figure>
+          <div className="text__container">
+            <h1 className="hero__mainText">
+              Analyze Music <span className="pinkText"> you enjoy</span> with no
+              limits<span className="pinkText">.</span>
+            </h1>
+            <h3 className="hero__subText">
+              Our extensive library makes it easy to unlock new discoveries
+              around the artists and genres you enjoy listening to most.
+            </h3>
+            <button className="purple__btn exploring__btn">
+              Start Exploring
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="custom-shape-divider-bottom-1665813386">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+      <div class="custom-shape-divider-top-1665813741">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;

050bc1d6d0d49a8300e97bc453552f6d0380cc82
jmadrigaladan <jorgemadrigaladan@gmail.com>
2022-10-17 16:21:39 -0700

    working on the discover section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Hero from "../components/Hero.jsx";
 import "./Home.css";
+import { Link } from "react-router-dom";
+import { ArrowForward } from "@material-ui/icons";
 
 function Home() {
   return (
@@ -12,6 +14,13 @@ function Home() {
             Let your passion for music bring you new{" "}
             <span className="purpleText">discoveries</span>
           </h1>
+          <p>
+            Let passion take the wheel as you uncover new artists, songs, and
+            genres. Our filters make it easy to find music that best suits your
+            taste.
+          </p>
+          <Link>Start discovering </Link>
+          <ArrowForward />
         </div>
       </section>
     </div>

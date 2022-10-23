255c207f22fb3a3f694b5a352bedb51c241607a8
jmadrigaladan <jorgemadrigaladan@gmail.com>
2022-10-22 23:51:52 -0700

    footer complete, landing page is missing responisveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./Footer.css";
 import logoWhite from "../assets/heardfrom text logo WHITE.svg";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer>
-      <img src={logoWhite} alt="" />
+      <div className="footer__container">
+        <img className="footer__image" src={logoWhite} alt="" />
+        <div className="footer__links--container">
+          <Link className="footer__link" to={"/"}>
+            Home
+          </Link>
+          <Link className="footer__link" to={"/analyzeMusic"}>
+            Analyze Music
+          </Link>
+          <Link className="footer__link" to={"/contact"}>
+            Contact
+          </Link>
+        </div>
+        <p className="copyright__text">Copyright &#169; 2022</p>
+      </div>
     </footer>
   );
 }
